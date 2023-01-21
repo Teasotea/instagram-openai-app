cecb54b47fb06c0956b434f5d9f50b06cf2c7021
Teasotea <sofi16616@gmail.com>
2023-01-21 17:09:55 +0200

    changed config settings

--- project/__init__.py
+++ project/__init__.py
@@ -12,16 +12,11 @@ def create_app():
 
     app.config['MAIL_SERVER']='smtp.mailtrap.io'
     app.config['MAIL_PORT'] = 2525
-    app.config['MAIL_USERNAME'] = '73a980d257ac94'
-    app.config['MAIL_PASSWORD'] = 'c9175449a1a75e'
+    app.config['MAIL_USERNAME'] = 'b4e19ec51f9c0b'
+    app.config['MAIL_PASSWORD'] = 'ee7add3db80dcd'
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = False
 
-    '''app.config['MAIL_SERVER']='smtp.gmail.com'
-    app.config['MAIL_PORT'] = 465
-    app.config['MAIL_USERNAME'] = 'your_email@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'your_password'
-    app.config['MAIL_USE_TLS'] = False'''
 
     db.init_app(app)
 
