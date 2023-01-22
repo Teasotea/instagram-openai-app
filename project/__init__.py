97b74db0f857960d450192590a14c1ca888d8984
Teasotea <sofi16616@gmail.com>
2023-01-22 13:44:44 +0200

    add gmail smtp for email verification

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
 
 db = SQLAlchemy()
 
@@ -10,13 +11,17 @@ def create_app():
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
-    app.config['MAIL_SERVER']='smtp.mailtrap.io'
-    app.config['MAIL_PORT'] = 2525
-    app.config['MAIL_USERNAME'] = 'b4e19ec51f9c0b'
-    app.config['MAIL_PASSWORD'] = 'ee7add3db80dcd'
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USE_SSL'] = False
 
+    mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": 'noreply.test222@gmail.com',#os.environ['EMAIL_USER'],
+    "MAIL_PASSWORD": 'ibxgnthlisddcbty'#os.environ['EMAIL_PASSWORD']
+    }   
+
+    app.config.update(mail_settings)
 
     db.init_app(app)
 
